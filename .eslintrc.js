d3e66c9211ed00856c3af57f7179394859290f88
zhuoyi <m2zhuoyi@163.com>
2020-10-21 14:27:12 +0800

    Some change

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-    page: true,
+    page: false,
     REACT_APP_ENV: true,
   },
 };
